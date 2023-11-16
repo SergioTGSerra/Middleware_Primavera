8153fcda322a9a48400e90ffd93b41cdba6c9f0c
Sergio Serra <sergioserra.2003@gmail.com>
2023-11-16 21:25:24 +0000

    Auth

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let token = null;
+let tokenExpirationTime = null;
+
+async function get_token() {
+  if (token && tokenExpirationTime && Date.now() < tokenExpirationTime)
+    return token;
+
+  const response = await fetch('https://identity.primaverabss.com/connect/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      'grant_type': 'client_credentials',
+      'scope': 'application',
+      'client_id': 'IEOP-API',
+      'client_secret': 'aa96e114-5991-424a-b124-574ebf836406',
+    }),
+  });
+
+  const data = await response.json();
+  token = data.access_token;
+  tokenExpirationTime = Date.now() + data.expires_in * 1000;
+  return token;
+}
+
+app.get('/', async (req, res) => {
+  const accessToken = await get_token();
+  res.send(accessToken);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
